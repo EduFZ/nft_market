d1f2a2b6387d20736523f37983a8034b59ebccef
Eduardo F Zimmermann <edu.zimmermann.efz@gamil.com>
2022-11-16 01:40:38 -0300

    Terminado de estilizar Section da Home.js

---
+++ src/common-components/Descriptions/HeadlineFour.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Colors, FontFamilies, FontSizes, FontWeights } from "../../shared/DesignTokens";
+
+export const HeadlineFour = styled.span`
+    font-family: ${FontFamilies.PRIMARY};
+    font-size: ${FontSizes.HEADLINE_4};
+    font-weight: ${FontWeights.BOLD};
+    color: ${Colors.WHITE};
+`;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,11 @@ import { Header } from "./../common-components/Header/Header";
 import BgImg from "./../assets/images/Background.png";
 import NftImg from "./../assets/images/Squares.png";
 import { Descriptions } from "./../common-components/Descriptions/Descriptions"
+import { Display } from "./../common-components/Descriptions/Display";
+import ImgLogo1 from "./../assets/icons/Logo1.png";
+import ImgLogo2 from "./../assets/icons/Logo2.png";
+import LogoTypes from "./../assets/icons/Logotypes.png";
+import { HeadlineFour } from "./../common-components/Descriptions/HeadlineFour";
 
 const Body = styled.div`
     width: 100%;
@@ -15,23 +20,91 @@ const Body = styled.div`
 
 const BodyTitle = styled.div `
     width: 100%;
+    margin: 165px 0 0 0;
     display: flex;
     justify-content: center;
 `;
 
 const Section = styled.div`
     width: 100%;
-    height: 1110px;
+    height: 670px;
     background-image: url(${NftImg});
     background-repeat: no-repeat;
-    border: 2px solid white;
 `;
 
-// const Squares = styled.img.attrs({src: NftImg, alt: 'Logotipo NFt Market'})`
-//     width: 1851px;
-//     height: 608px;
-//     margin-top: 200px;
-// `;
+const Title = styled.div`
+    width: 100%:
+    height: 219px;
+    margin: 34px 0 0 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+const TitleBoxOne = styled.span`
+    width: 837px;
+    height: 120px;
+    display: flex;
+    align-items: center;
+`;
+
+const TitleBoxTwo = styled.span`
+    width: 837px;
+    height: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+`;
+
+const TextTwo = styled.span`
+    width: 100%;
+    height: 36px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 37px 0 0 0;
+`;
+
+const Img1 = styled.img.attrs({src: ImgLogo1, alt: 'Logotipo NFt Market'})`
+    width: 40px;
+    height: 91px;
+`;
+
+const Img2 = styled.img.attrs({src: ImgLogo2, alt: 'Logotipo NFt Market'})`
+    width: 62px;
+    height: 62px;
+`;
+
+const DivLogos = styled.div`
+    width: 100%;
+    height: 45px;
+    margin: 50px 0 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Logos = styled.img.attrs({src: LogoTypes, alt: 'Logomarcas'})`
+    height: 45px;
+`;
+
+const TitleTwo = styled.span`
+    width: 100%;
+    height: 45px;
+    margin: 65px 0 72px 0;
+    display: flex;
+    justify-content: center;
+    align-items: 45px;
+`;
+
+const DivCards = styled.div`
+    width: 100%;
+    height: 767px;
+    display: flex;
+`;
+
+
+
 
 export function Home() {
     return (
@@ -42,8 +115,32 @@ export function Home() {
                     <Descriptions>NON FUNGIBLE TOKENS</Descriptions>
                 </BodyTitle>
                 <Section>
+                    <Title>
+                        <TitleBoxOne>
+                            <Display>A new NFT</Display>
+                            <Img1 />
+                        </TitleBoxOne>
+
+                        <TitleBoxTwo>
+                            <Img2 />
+                            <Display>Experience</Display>
+                        </TitleBoxTwo>
+                    </Title>
+
+                    <TextTwo>
+                        <Descriptions>Discover, collect and sell</Descriptions>
+                    </TextTwo>
 
                 </Section>
+
+                <DivLogos>
+                    <Logos />
+                </DivLogos>
+
+                <TitleTwo>
+                    <HeadlineFour>Latest live auctions</HeadlineFour>
+                </TitleTwo>
+
             </Body>
         </div>
     );
